b2b49a9d0265fc13205d9ba696972bd56e89f9d7
Balaji Ramesh <balajiramesh@vt.edu>
2020-03-05 21:59:52 -0500

    passin commit error

--- analysis.R
+++ analysis.R
@@ -168,8 +168,8 @@ df$imperInd<-df$imperInd/5.0
 
 print(indes)
 indes <- c(
-            'flooded','electricity','otherHomesFlood','skinContact',
-            'leftHome',
+            # 'flooded','electricity','otherHomesFlood','skinContact',
+            # 'leftHome',
             'SVI','floodRatio','imperInd'
             # 'waterLevelC_2','waterLevelC_4','waterLevelC_8',
              # 'electricityLostDaysC_18','electricityLostDaysC_30','electricityLostDaysC_90'
