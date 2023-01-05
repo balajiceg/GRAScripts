08b54e4587af9ca75fd7699fcf797e3c570b7ad8
Ramesh <balaji.9th@gmail.com>
2023-01-04 22:58:47 -0500

    04012023_3 Level of analysis: ct Flooding used : overall % Extent: dfo Flood product: aer Design: pre post control Flood categoris: 2 - array([0.000000e+00, 1.000000e-06, 9.930878e-01]) Outcome: Dehydration

--- dhs_scripts/OverdoesPriliminaryAnalysis.py
+++ dhs_scripts/OverdoesPriliminaryAnalysis.py
@@ -148,7 +148,7 @@ sp_bkp = sp.copy()
 #expsoure level ct or bg (ct-census tract; bg- blockgroup)
 EXPOSURE_LEVEL = 'ct' 
 #exposure product dfo or aer
-EXPOSURE_PRODUCT = 'dfo'
+EXPOSURE_PRODUCT = 'aer'
 #type of flooding fRatio or fldResRatio (fRatio - overall flood ratio; fldResRatio - residential flooding ratio) 
 FLOOD_TYPE = 'fRatio'
 #extent of cenus tracts defined using which flood product extent : dfo or aer
