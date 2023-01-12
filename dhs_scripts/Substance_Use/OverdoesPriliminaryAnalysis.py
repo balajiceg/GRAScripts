3f9cab6f6048d71132387c14f54d957f90906409
Ramesh <balaji.9th@gmail.com>
2023-01-12 10:28:57 -0500

    18

--- dhs_scripts/Substance_Use/OverdoesPriliminaryAnalysis.py
+++ dhs_scripts/Substance_Use/OverdoesPriliminaryAnalysis.py
@@ -167,7 +167,7 @@ EXPOSURE_LEVEL = 'bg'
 #exposure product dfo or aer
 EXPOSURE_PRODUCT = 'dfo'
 #type of flooding fRatio or fldResRatio (fRatio - overall flood ratio; fldResRatio - residential flooding ratio) 
-FLOOD_TYPE = 'fRatio'
+FLOOD_TYPE = 'fldResRatio'
 #extent of cenus tracts defined using which flood product extent : dfo or aer
 EXTENT_ANALYSIS = 'dfo'
 
